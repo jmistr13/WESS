1b092e38f5c484f8c41bddb701368972650cda27
loganma02 <loganm02@uw.edu>
2025-02-25 19:07:57 -0800

    commiting before pull

--- Wireless/rpi_uart_lora.py
+++ Wireless/rpi_uart_lora.py
@@ -2,6 +2,7 @@ import serial
 import time
 import csv
 import re
+from datetime import datetime
 
 lora = serial.Serial(
 	port="/dev/ttyAMA0",
@@ -18,13 +19,19 @@ time.sleep(1)
 
 with open("readings.csv", 'w', newline='') as file:
 	writer = csv.writer(file)
-	writer.writerow(["CO","NO2","NH3","TDS"])
+	writer.writerow(["transmitDateTime", "CO","NO2","NH3","TDS"])
 	
 	while True:
 		msg = lora.readline()
 		print(msg.decode(errors="ignore").strip())
 		vals = re.findall(r"[-+]?(?:\d*\.\d+)", msg.decode())
+		
+		now = datetime.now()
+		date_time_str = now.strftime("%Y-%m-%d %H:%M:%S")
+		vals.insert(0, date_time_str)
+		print(vals)
 		writer.writerow(vals)
+		file.flush()
 		time.sleep(5)
 
 lora.close()
