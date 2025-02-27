d1afda97cb9b12a29b0509cd95e80736274728fc
loganma02 <loganm02@uw.edu>
2025-02-26 22:27:21 -0800

    Python code for handling +OK and +ERR messages and properly parsing into CSV

--- Wireless/rpi_uart_lora.py
+++ Wireless/rpi_uart_lora.py
@@ -19,6 +19,7 @@ time.sleep(1)
 
 with open("readings.csv", 'w', newline='') as file:
 	writer = csv.writer(file)
+<<<<<<< HEAD
 	writer.writerow(["sensorName","lat","long","transmitDateTime","CO","NO2","NH3","TDS","turbidity"])
 	
 	while True:
@@ -36,5 +37,29 @@ with open("readings.csv", 'w', newline='') as file:
 		writer.writerow(vals)
 		file.flush() #Saves the file
 		time.sleep(5)
+=======
+	writer.writerow(["sensorName","lat","long","transmitDateTime", "CO","NO2","NH3","TDS","turbidity"])
+	
+	while True: #maybe change this to when theres an actual line to read?
+		if lora.in_waiting > 0:
+			msg = lora.readline()
+			if "+OK" in msg.decode() or "+ERR" in msg.decode():
+				continue
+			print(msg.decode(errors="ignore").strip())
+			vals = re.findall(r"[-+]?(?:\d*\.\d+)", msg.decode())
+			now = datetime.now()
+			date_time_str = now.strftime("%Y-%m-%d %H:%M:%S")
+			vals.insert(2, date_time_str)
+			sensor_name = msg.decode().split(",")
+			if (len(sensor_name) > 1):
+				sensor_name = sensor_name[2]
+				vals.insert(0, sensor_name)
+			print(vals)
+			writer.writerow(vals)
+			file.flush()
+			time.sleep(1)
+		else:
+			time.sleep(1)
+>>>>>>> f0dec39 (Python code for handling +OK and +ERR messages and properly parsing into CSV)
 
 lora.close()
