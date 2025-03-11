ffee1ca01f23e68c0587613e1332483a016be66e
Jesse Mistr <116523713+jmistr13@users.noreply.github.com>
2025-03-10 18:53:25 -0700

    Update rpi_uart_lora.py

--- DataAnalysis/appDev/rpi_uart_lora.py
+++ DataAnalysis/appDev/rpi_uart_lora.py
@@ -9,7 +9,7 @@ lora = serial.Serial(
 	baudrate=115200,
 	timeout=1,
 	)
-		
+
 lora.write(str.encode("AT+BAND=915000000\r\n"))
 time.sleep(1)
 lora.write(str.encode("AT+NETWORK_ID=5\r\n"))
@@ -19,22 +19,27 @@ time.sleep(1)
 
 with open("readings.csv", 'w', newline='') as file:
 	writer = csv.writer(file)
-	writer.writerow(["sensorName","lat","long","transmitDateTime","CO","NO2","NH3","TDS","turbidity"])
-	
-	while True:
-		msg = lora.readline()
-		print(msg.decode(errors="ignore").strip())
-		vals = re.findall(r"[-+]?(?:\d*\.\d+)", msg.decode())
-		
-		now = datetime.now()
-		date_time_str = now.strftime("%Y-%m-%d %H:%M:%S")
-		vals.insert(1, date_time_str) #Insert transmitDateTime
-		vals.insert(1, "") #Insert long
-		vals.insert(1, "") #Insert lat
+	writer.writerow(["sensorName","lat","long","transmitDateTime", "CO","NO2","NH3","TDS","turbidity"])
 
-		print(vals)
-		writer.writerow(vals)
-		file.flush() #Saves the file
-		time.sleep(5)
+	while True: #maybe change this to when theres an actual line to read?
+		if lora.in_waiting > 0:
+			msg = lora.readline()
+			if "+OK" in msg.decode() or "+ERR" in msg.decode():
+				continue
+			print(msg.decode(errors="ignore").strip())
+			vals = re.findall(r"[-+]?(?:\d*\.\d+)", msg.decode())
+			now = datetime.now()
+			date_time_str = now.strftime("%Y-%m-%d %H:%M:%S")
+			vals.insert(2, date_time_str)
+			sensor_name = msg.decode().split(",")
+			if (len(sensor_name) > 1):
+				sensor_name = sensor_name[2]
+				vals.insert(0, sensor_name)
+			print(vals)
+			writer.writerow(vals)
+			file.flush()
+			time.sleep(1)
+		else:
+			time.sleep(1)
 
 lora.close()
